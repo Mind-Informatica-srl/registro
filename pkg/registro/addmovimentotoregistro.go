485de7474eb8e7b4841845827b4e023f75d33a7c
fabiodev30 <107891791+fabiodev30@users.noreply.github.com>
2022-11-23 11:58:54 +0100

    fixato counter in add movimento registro e aggiunto test

--- pkg/registro/addmovimentotoregistro.go
+++ pkg/registro/addmovimentotoregistro.go
@@ -10,15 +10,16 @@ import (
 func (re *Registro) AddMovimentoToRegistro(movimento *Movimento) (err error) {
 	// assegno l'id al movimento
 	re.counter++
-	movimento.ID = re.counter
-	movimento.DataInserimento = time.Now()
 	if len(re.ListaMovimenti) == 0 {
 		if movimento.TipoMovimento != "carico" {
 			err = errors.New("Il primo elemento dell'array non può essere uno scarico")
-			panic(err)
+			re.counter = 0
+			return err
 		}
 
 	}
+	movimento.ID = re.counter
+	movimento.DataInserimento = time.Now()
 	// appendo il nuovo movimento alla lista dei movimenti del registro
 	re.ListaMovimenti = append(re.ListaMovimenti, *movimento)
 	// dato che nuovo elemento è stato inserito riordino lista elementi del registro
@@ -30,11 +31,13 @@ func (re *Registro) AddMovimentoToRegistro(movimento *Movimento) (err error) {
 	err = errore
 	if err != nil {
 		err = re.EliminareMovimentoToRegistro(movimento.ID)
+		re.counter--
 		if err != nil {
 			return err
 		}
-		stringaerrore := fmt.Sprintf("Movimento %d non è stato possibile inserirlo dato che la quantita del saldo è minore di quella dello scarico", movimento.ID)
-		panic(errors.New(stringaerrore))
+		stringaerrore := fmt.Sprintf("Movimento con id %d non è stato possibile inserirlo dato che la quantita del saldo è minore di quella dello scarico", movimento.ID)
+		err = errors.New(stringaerrore)
+		return err
 	}
 	fmt.Println(saldo)
 	return
